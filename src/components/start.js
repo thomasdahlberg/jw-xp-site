b8215e86fc54ee884d8bbf6c05a843a894e3eb94
Thomas Dahlberg <thomas.dahlberg8@gmail.com>
2020-09-29 22:48:32 -0400

    initializes start component

---
+++ src/components/start.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class Start extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return(
+            <div>
+                Start Menu Component
+            </div>
+        )
+    }
+
+}
+
+export default Start;
